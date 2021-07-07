7d74eee598ae6657a45c75907c957ea7362e685c
wangyifan <980821704@qq.com>
2021-07-07 18:02:48 +0800

    开启RestTemplate负载均衡

--- cloud-consumer-order80/src/main/java/com/wyf/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/wyf/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.wyf.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,9 +12,11 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContextConfig {
     /**
      * 添加进spring容器
+     * @LoadBalanced 开启负载均衡
      * @return 返回一个RestTemplate对象
      */
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
