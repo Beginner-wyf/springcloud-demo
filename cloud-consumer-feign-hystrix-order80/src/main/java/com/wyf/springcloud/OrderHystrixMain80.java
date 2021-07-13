b9cd1e645dad8732bff7be282e27a76d20fc9294
wangyifan <980821704>
2021-07-13 09:51:42 +0800

    Hystrix:高并发测试

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/wyf/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,16 @@
+package com.wyf.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author wangyifan
+ */
+@EnableFeignClients
+@SpringBootApplication
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class, args);
+    }
+}
